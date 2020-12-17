a87af8213759a37e8831b653be273dfe946c82b3
0123Lixinying <2606371028@qq.com>
2020-12-17 20:28:26 +0800

    一次

---
+++ app/src/main/java/com/example/dapiaoliang/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dapiaoliang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //客户看三十个
+    }
+}
